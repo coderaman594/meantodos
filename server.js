83dc586a63a65c7781041ed50ea450bf358bc7bf
Aman Suryawanshi <aman.s@socialpanga.com>
2022-01-13 15:14:04 +0530

    [2022-01-13 15:14:04] feat- server.js setup done

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
-var index = require('./routes/index');
-var todos = require('./routes/todos');
+var index = require("./routes/index");
+var todos = require("./routes/todos");
+const { append } = require("express/lib/response");
 
 // View Engine
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ urlencoded: false }));
+
+app.use("/", index);
+app.use("/api/v1", todos);
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000...s");
+});
