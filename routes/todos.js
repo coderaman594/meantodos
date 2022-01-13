2d604ba0be8a02deba50830796a550946f86b70b
Aman Suryawanshi <suryawanshi.aman1994@gmail.com>
2022-01-13 21:47:48 +0530

    [2022-01-13 21:47:48] feat- mongodb connection issue resolved

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongojs = require('mongojs');
-var db = mongojs('mongodb+srv://coderaman594:3iDJvRcze0Q51Af5@cluster0.ffr5w.mongodb.net/meantodo_aman_dev?retryWrites=true&w=majority',['todos']);
+var mongojs = require("mongojs");
+var db = mongojs(
+    "mongodb+srv://coderaman594:3iDJvRcze0Q51Af5@cluster0.ffr5w.mongodb.net/meantodo_aman_dev?retryWrites=true&w=majority",
+    ["todos"]
+);
 
-router.get('/todos', function (req, res, next) {
+router.get("/todos", function (req, res, next) {
     db.todos.find(function (err, todos) {
         if (err) {
             res.send(err);
