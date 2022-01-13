6bd9e7cd701e5e15b61a5b9a89e7c1679736b49a
Aman Suryawanshi <suryawanshi.aman1994@gmail.com>
2022-01-13 20:10:16 +0530

    [2022-01-13 20:10:16] feat-mongodb connections work

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb+srv://coderaman594:3iDJvRcze0Q51Af5@cluster0.ffr5w.mongodb.net/meantodo_aman_dev?retryWrites=true&w=majority',['todos']);
 
-router.get("/todos", function (req, res, next) {
-    res.send("TODOS API");
+router.get('/todos', function (req, res, next) {
+    db.todos.find(function (err, todos) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(todos);
+        }
+    });
 });
 
 module.exports = router;
