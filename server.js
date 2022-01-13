b654eef708adffdff81564bc944ffca3e28782d0
Aman Suryawanshi <aman.s@socialpanga.com>
2022-01-13 15:05:40 +0530

    [2022-01-13 15:05:40] feat- server js coding

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+
+// View Engine
+
