cb65aef6bf6e55669c13fc0494ea66da430d9edb
Aman Suryawanshi <suryawanshi.aman1994@gmail.com>
2022-01-14 18:54:49 +0530

    [2022-01-14 18:54:49] feat-crud mongo apis

--- routes/todos.js
+++ routes/todos.js
@@ -16,7 +16,7 @@ router.get("/todos", function (req, res, next) {
     });
 });
 
-// Get Single ToDO
+// Get Single ToDo
 router.get("/todo/:id", function (req, res, next) {
     db.todos.findOne(
         {
@@ -27,6 +27,73 @@ router.get("/todo/:id", function (req, res, next) {
                 res.send(err);
             } else {
                 res.json(todo);
+            }
+        }
+    );
+});
+
+// Save ToDo
+router.post("/todo/", function (req, res, next) {
+    var todo = req.body;
+    if (!todo.next || !(todo.isCompleted + "")) {
+        res.status("400");
+        res.json({ error: "Invalid ToDo Data" });
+    } else {
+        db.save(todo, function (err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+    }
+});
+
+// Update ToDo
+router.put("/todo/:id", function (req, res, next) {
+    var todo = req.body;
+    var updObj = {};
+    if (todo.isCompleted) {
+        updObj.isCompleted = todo.isCompleted;
+    }
+    if (todo.text) {
+        updObj.text = todo.text;
+    }
+
+    if (!updObj) {
+        res.status("400");
+        res.json({ error: "Invalid ToDo Data" });
+    } else {
+        db.todos.update(
+            {
+                _id: mongojs.ObjectId(req.params.id),
+            },
+            updObj,
+            {},
+            function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.json(result);
+                }
+            }
+        );
+    }
+});
+
+// Delete ToDo
+router.delete("/todo/:id", function (req, res, next) {
+    db.todos.remove(
+        {
+            _id: mongojs.ObjectId(req.params.id),
+        },
+        updObj,
+        "",
+        function (err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(result);
             }
         }
     );
